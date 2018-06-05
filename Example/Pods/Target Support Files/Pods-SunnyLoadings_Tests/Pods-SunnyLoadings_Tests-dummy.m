2fbdac129622464912e31cb18ce1612a51abc9c8
Sunny Ouyang <ShenOuyang.work@gmail.com>
2018-06-04 22:09:35 -0700

    first commit

---
+++ Example/Pods/Target Support Files/Pods-SunnyLoadings_Tests/Pods-SunnyLoadings_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SunnyLoadings_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_SunnyLoadings_Tests
+@end
---
+++ Example/Pods/Target Support Files/SunnyLoadings/SunnyLoadings-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SunnyLoadings : NSObject
+@end
+@implementation PodsDummy_SunnyLoadings
+@end
